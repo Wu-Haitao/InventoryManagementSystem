df96b020ac5306c8ecd787d31ae35e92f0bdac52
Haitao <wuhaitao99.st@gmail.com>
2021-08-24 10:28:11 +0800

    Solve the bug in History

--- src/main/java/com/example/InventoryManagementSystem/History.java
+++ src/main/java/com/example/InventoryManagementSystem/History.java
@@ -4,8 +4,10 @@ import java.util.Stack;
 
 public class History {
     private static Stack<String> history = new Stack<>();
+    private static boolean flag = false;
 
     public static void goForward(String newAssetTag) {
+        flag = true;
         history.push(newAssetTag);
     }
 
@@ -13,9 +15,12 @@ public class History {
         if (history.lastElement().equals("root")) {
             return "root";
         }
-        history.pop();
-        if (history.lastElement().equals("root")) {
-            return "root";
+        if (flag) {
+            flag = false;
+            history.pop();
+            if (history.lastElement().equals("root")) {
+                return "root";
+            }
         }
         return history.pop();
     }
